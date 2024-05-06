c088f8652fd8902ea7aa8d0c6f748a2eb07aa86c
khaoula <bouzartikhaoula@gmail.com>
2024-05-06 13:50:08 +0200

    problem_solving_in_js tow project

---
+++ challeng01_even_OrOdd.js
@@ -0,0 +1,21 @@
+// code01
+function evenorodd(num) {
+    if (num % 2===0) {
+return "even"
+    }else{
+return "odd" 
+   }
+}
+console.log(evenorodd(4));
+console.log(evenorodd(5));
+
+console.log("/".repeat(15));
+
+// code02
+
+function evenorodd(num) {
+   return  num % 2===0? "even" :"odd"
+    
+}
+console.log(evenorodd(4));
+console.log(evenorodd(5));
---
+++ challeng02_SumOfPositives.js
@@ -0,0 +1,30 @@
+//code1
+function evenorodd(arry) {
+    let sum=0;
+    for (let i = 0; i < arry.length; i++) {
+
+    if (arry[i] % 2===0) {
+    sum=sum+arry[i] ;
+    }else{
+    sum=sum+0;  }
+    }
+    console.log(sum);
+}
+evenorodd([1,4,8,9,2])//defrant 
+evenorodd(5);// one valeu 
+evenorodd([1,3,7,9,1]) //all negative 
+evenorodd([10,10,10,10,2])// all positive value
+
+console.log("#".repeat(10));
+//code2
+function name(num) {
+    let sum
+    {arry[i] % 2===0?    sum=sum+arry[i] : sum=sum+0;}
+    console.log(sum);
+
+}
+
+evenorodd([1,4,8,9,2])//defrant 
+evenorodd(5);// one valeu 
+evenorodd([1,3,7,9,1]) //all negative 
+evenorodd([10,10,10,10,2])// all positive value
