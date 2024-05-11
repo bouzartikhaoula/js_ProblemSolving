cd268fb79fd27e827d52ad7d80c46732852356ab
khaoula <bouzartikhaoula@gmail.com>
2024-05-11 13:13:08 +0200

    Jenny's secret

---
+++ challeng09_Jenny's_secret.js
@@ -0,0 +1,21 @@
+// Error
+// function greet(name) {
+//     return"Hello," +name+!;
+//     if (name==="johnny") {
+//         return"Hello, my love !";
+//     }
+// }
+
+
+// true
+
+
+function greet(name) {
+    
+    if (name==="johnny") {
+        return"Hello, my love !";
+    }
+    return"Hello," +name+"!";
+}
+console.log(greet("johnny"));
+console.log(greet("khaoula"));
