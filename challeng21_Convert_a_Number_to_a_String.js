a4f01c548e8d242067f20d6e04a09fdcb989cb0f
khaoula <bouzartikhaoula@gmail.com>
2024-05-24 00:54:29 +0200

    Convert a Number to a String

---
+++ challeng21_Convert_a_Number_to_a_String.js
@@ -0,0 +1,13 @@
+
+//code 1
+function toString(elem) {
+    return elem.toString()
+}
+console.log(typeof toString(2));
+
+//code 2
+
+function intToString(elem) {
+    return `${elem}`
+}
+console.log(typeof intToString(2));
