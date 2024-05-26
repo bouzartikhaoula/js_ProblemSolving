81cdf53d15c1e71cbe7105018eeb4cdd0982f4dd
khaoula <bouzartikhaoula@gmail.com>
2024-05-26 18:27:40 +0200

    Convert a String to a Number

---
+++ challeng23Convert_String_ToaNumber.js
@@ -0,0 +1,12 @@
+// code 1
+function SgtoInt(elem) {
+    return Number(elem)
+}
+
+console.log(SgtoInt("3"));
+// code 2
+function SgtoInt2(elem) {
+    return parseInt(elem)
+}
+
+console.log(SgtoInt2("3"));
