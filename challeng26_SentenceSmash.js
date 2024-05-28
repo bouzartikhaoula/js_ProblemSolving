9a267680a3cdfb7ea5fa0aba93cf1dc7fd2d6410
khaoula <bouzartikhaoula@gmail.com>
2024-05-28 13:02:31 +0200

    Sentence Smash

---
+++ challeng26_SentenceSmash.js
@@ -0,0 +1,7 @@
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+function sentenceS(array) {
+     
+return [array.join(" ")]
+}
+
+console.log(sentenceS( ['hello', 'world', 'this', 'is', 'great']));
