f9b99757f3e0fd755faf1a2487f4dd396b4abc6c
khaoula <bouzartikhaoula@gmail.com>
2024-05-31 00:24:37 +0200

    last update

--- challeng29-ExclamationMarks.js
+++ challeng29-ExclamationMarks.js
@@ -1,10 +1,16 @@
+
+// code1
 function Marks(elemnt) {
 
     let f= elemnt.split("")
     if (f[f.length-1]=="!") {
-    console.log(f.slice(0,f.length-1));
+   return f.slice(0,f.length-1).join("");
     }
     
 }
 console.log( Marks("khju !"));
-
+// code2
+function Marks2(props) {
+   return props.replace(/!$/,"")
+}
+console.log( Marks2("k!hju!"));//k!hju
