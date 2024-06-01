8bd19f7d03b91744057d0b17c5baa05298a14727
khaoula <bouzartikhaoula@gmail.com>
2024-06-01 20:14:24 +0200

    code 2

--- challeng31_RemovingElements.js
+++ challeng31_RemovingElements.js
@@ -9,6 +9,10 @@ for (let i = 0; i < e.length; i++) {
 }
 return s;
 }
-console.log(Remove(["z1","kj","z2","kj","z3","kj"]));
+console.log(Remove(["z1","kj","z2","kj","z3","kj"]));// ['z1', 'z2', 'z3']
 
 
+function Remove2(f) {
+return f.filter( ( e,i)=> i % 2===0)
+}
+console.log(Remove2(["z1","kj","z2","kj","z3","kj"]));// ['z1', 'z2', 'z3']
