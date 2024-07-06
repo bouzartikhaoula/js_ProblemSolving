0e1fa73a2df00f3be1055b7403a3ed3135d81850
khaoula <bouzartikhaoula@gmail.com>
2024-07-06 17:37:40 +0200

    Opposites attract challeng 40

---
+++ challeng40_Opposites attract.js
@@ -0,0 +1,13 @@
+function Opposites(a,b) {
+
+    // if ((a%2==0 && b%2!==0) || (a%2!==0 && b%2==0)) {
+    //     return true
+    // }else{
+    //     return false
+    // }
+ return    a%2 !== b%2
+}
+
+
+
+console.log(Opposites(3,6));
